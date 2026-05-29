7b6415bdb1693bcb8e23fbbdc1fe76b5e3767092
Amrith Gandham <amrithgandham@gmail.com>
2026-05-29 15:27:04 -0700

    changes to footer

--- frontend/src/app/components/Footer.tsx
+++ frontend/src/app/components/Footer.tsx
@@ -4,12 +4,12 @@ import uwMedicineLogo from "../../assets/uw_medicine_logo.png";
 export function Footer() {
   return (
     <footer className="bg-[#262626] text-white px-8 py-10">
-      <div className="flex flex-col md:flex-row gap-8 md:gap-12 items-start">
+      <div className="mx-auto flex flex-col md:flex-row gap-8 md:gap-12 items-center">
         <div className="flex flex-col gap-6 flex-shrink-0 items-center">
           <img
             src={ithsLogo}
             alt="Institute of Translational Health Sciences"
-            className="h-14 w-auto object-contain"
+            className="h-16 w-auto object-contain"
           />
           <img
             src={uwMedicineLogo}
@@ -23,12 +23,12 @@ export function Footer() {
             About PeerLink
           </h3>
           <p className="text-sm leading-relaxed text-white/80 max-w-2xl">
-            PeerLink is a reviewer matching tool developed in collaboration with
-            the Institute of Translational Health Sciences (ITHS) and a group of
-            students at the Information School (iSchool) at the University of
-            Washington. This project was developed as a part of the iSchool's
-            Capstone Program in 2026. To learn more about the iSchool's Capstone
-            Program, please visit{" "}
+            PeerLink was developed in collaboration with the Institute of
+            Translational Health Sciences (ITHS) and a group of students at the
+            Information School (iSchool) at the University of Washington. This
+            project was developed as a part of the iSchool's Capstone Program in
+            2026. To learn more about the iSchool's Capstone Program, please
+            visit{" "}
             <a
               href="https://ischool.uw.edu/capstone"
               target="_blank"
@@ -36,8 +36,8 @@ export function Footer() {
               className="text-blue-400 hover:underline"
             >
               https://ischool.uw.edu/capstone.
-            </a>
-            {" "}This website is currently maintained by{" "}
+            </a>{" "}
+            This website is currently maintained by{" "}
             <a
               href="https://rit.uw.edu/"
               target="_blank"
@@ -45,8 +45,9 @@ export function Footer() {
               className="text-blue-400 hover:underline"
             >
               UW Medicine Research IT.
-            </a>
-            {" "} <br /> Created by Amrith Gandham, Aradhana Muthabatulla, Liya Hutchison,
+            </a>{" "}
+            <br />
+            Created by Amrith Gandham, Aradhana Muthabatulla, Liya Hutchison,
             Rohan Simha, and Mykyta Lepikash.
           </p>
           <p className="text-xs text-white/50 mt-4">
